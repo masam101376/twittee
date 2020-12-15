ead641ae7624a5bbd42aeddcdaab1a871af5fba4
masam101376 <ma.sa.m10.1.3.7.6@gmail.com>
2020-12-15 16:28:28 +0900

    Rename the project

--- twittee/wsgi.py
+++ twittee/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for twitee project.
+WSGI config for twittee project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twitee.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twittee.settings')
 
 application = get_wsgi_application()
