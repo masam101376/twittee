615de0245f9af2a94132a27cc927bb543045debe
masam101376 <ma.sa.m10.1.3.7.6@gmail.com>
2020-12-15 18:18:31 +0900

    Reflect .gitignore

--- user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.urls.resolvers import URLPattern
-from . import views
-
-app_name = 'user'
-
-urlpatterns = [
-    path('home/', views.HomeView.as_view(), name='home')
-]
